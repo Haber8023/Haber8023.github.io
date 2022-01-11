8c7b6826aefe6dd186234ff34b353763f5f55ba5
Haber8023 <664037252@qq.com>
2022-01-12 01:03:09 +0800

    Site updated: 2022-01-12 01:03:08

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+window.addEventListener("DOMContentLoaded", function() {
   const html            = document.querySelector("html");
   const navBtn          = document.querySelector(".navbar-btn");
   const navList         = document.querySelector(".navbar-list");
